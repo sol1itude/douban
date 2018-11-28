828142c2d7dec485085b3e2bf7e3d35fcca14495
sol1itude <wangfengjie123@gmail.com>
2018-11-28 11:31:15 +0800

    豆瓣

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <button type="button" @click="axios">axios</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    axios(){
+      this.$http.get('/api/movie/in_theaters').then( res => {
+        console.log(res);
+      }).catch( err => {
+        console.log(err);
+      });
+    }
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='scss'>
+.hello{
+  p{
+    color: red;
+  }
+}
+</style>
